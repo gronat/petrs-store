3082133e3ff827bcca736cfc33f3c220214290df
gronat <petr.gronat@oakslab.com>
2025-03-17 22:22:48 +0100

    prod config

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path"
 
 dotenv.config();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist/")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running at http://localhost:" + PORT);
