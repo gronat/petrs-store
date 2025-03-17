d23fdf8591b7aa35bad26213be25047938d9f3ce
gronat <petr.gronat@oakslab.com>
2025-03-17 23:24:58 +0100

    mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("Got mongo URI = ", process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } 
